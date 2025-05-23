5f350116c4032b39e1d22ca2000facfadc561b88
tdombui <tdombui@gmail.com>
2025-05-23 14:38:25 -0700

    strict:false

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "BentoWise",
+  title: "Bentowise",
   description: "Lightweight Shop Management Platform with Operational Gravity",
 };
 
